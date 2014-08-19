f1a2e9dd6640aba69d4d36e0d16b28ca5efcb8c6
Robert Obryk <robryk@gmail.com>
2014-08-19 10:22:58 +0200

    bank, first cut

---
+++ bank/bank.cpp
@@ -0,0 +1,83 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include "bench.h"
+#include <cassert>
+
+using namespace std;
+
+constexpr int n_accounts = 1000;
+
+mutex mu;
+int values[n_accounts];
+
+void init() {
+	for(int i=0;i<n_accounts;i++)
+		values[i] = 0;
+}
+
+void add(int to, int amount) {
+	assert(amount >= 0);
+	mu.lock();
+	values[to] += amount;
+	mu.unlock();
+}
+
+bool txfr(int from, int to, int amount) {
+	bool ret = false;
+	assert(amount >= 0);
+	mu.lock();
+	if (values[from] >= amount) {
+		values[from] -= amount;
+		values[to] += amount;
+		ret = true;
+	}
+	mu.unlock();
+	return ret;
+}
+
+int read(int accnt) {
+	mu.lock();
+	int r = values[accnt];
+	mu.unlock();
+	return r;
+}
+
+// IMPLEMENTACJA POWYZEJ
+// TESTY PONIZEJ
+
+void random_thread(B* b) {
+	while (!b->should_stop()) {
+		txfr(rand() % n_accounts, rand() % n_accounts, rand() % 1000);
+		b->inc();
+	}
+}
+
+void bench_random(int n_threads) {
+	thread thr[n_threads];
+	B b;
+	init();
+	int total_before = 0;
+	for(int i=0;i<n_accounts;i++) {
+		add(i, 2000);
+		total_before += 2000;
+	}
+	for(int i=0;i<n_threads;i++)
+		thr[i] = thread(&random_thread, &b);
+	for(int i=0;i<n_threads;i++)
+		thr[i].join();
+	int total_after = 0;
+	for(int i=0;i<n_accounts;i++) {
+		total_after += read(i);
+	}
+	assert(total_before == total_after);
+	cout << n_threads << " threads, random - " << b.report() << " ops/us\n";
+}
+
+int main() {
+	bench_random(1);
+	bench_random(2);
+	return 0;
+}
+
+
